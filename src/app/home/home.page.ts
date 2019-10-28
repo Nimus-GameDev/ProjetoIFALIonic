635c3e175d1cd3fe9c266e67eef8af1db6b90ea4
Mikhael D'Amato <damato578@gmail.com>
2019-10-28 14:32:53 -0300

    Menu e Favorites Adicionados

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -11,26 +10,6 @@ export class HomePage {
   public iconSquare = '../../assets/images/bntSquare.png';
   public iconStar = '../../assets/images/bntStar.png';
 
-  constructor(public toast: ToastController) {}
-
-  public async squareClick() {
-
-    const t = await this.toast.create({message: 'Square was actived.',
-                                              duration: 500,
-                                              position: 'middle',
-                                              color: 'secondary'});
-    t.present();
-
-  }
-
-  public async starClick() {
-
-    const t = await this.toast.create({message: 'Star was actived.',
-                                              duration: 500,
-                                              position: 'middle',
-                                              color: 'success'});
-    t.present();
-
-  }
+  constructor() {}
 
 }
